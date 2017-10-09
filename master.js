cec6076f384c7b5abc1e0feee88b155ad0004000
Shuhao Chen <shuhao.chen@sjsu.edu>
2017-10-09 10:07:01 -0700

    addScreenScroll

--- master.js
+++ master.js
@@ -1,4 +1,25 @@
-$("#btn").on("click",function(e){
-  e.preventDefault();
-  $("body, html").animate({scrollTop: 0}, 1300);
+$("#btn").on("click", function(e) {
+    e.preventDefault();
+    $("body, html").animate({
+        scrollTop: 0
+    }, 1300);
 });
+$(document).ready(function() {
+    $(window).on("load", function() {
+        window.onscroll = function() {
+            if (getheight()) {
+                $(".botton").css("display", "flex");
+            }else {
+                $(".botton").css("display","none");
+            }
+        }
+    });
+});
+
+function getheight() {
+    var box = $(".box");
+    var totalhtight = box.last().get(0).offsetTop +Math.floor(box.last().height()) / 2;
+    var documentheight = $(document).width();
+    var scrollheight = $(window).scrollTop();
+    return (totalhtight < documentheight + scrollheight) ? true : false;
+};
